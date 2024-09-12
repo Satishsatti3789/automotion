133bd9f8d2121c70d5a45b8fee93413d72a2d6ea
Satishsatti3789 <satishdevops3789@gmail.com>
2024-09-12 08:56:15 +0530

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -3,12 +3,12 @@
 export LOG=/tmp/awsauto.log
 rm -f $LOG
 
-echo -e "\n\e[1;33m You can find all the scripts in following location\e[0m\nhttps://github.com/learndevopsonline/labautomation/tree/master/aws\n"
+echo -e "\n\e[1;33m You can find all the scripts in following location\e[0m\nhttps://github.com/satishsatti3789/automation/tree/master/aws\n"
 
-source /tmp/labautomation/dry/common-functions.sh
+source /tmp/automation/dry/common-functions.sh
 
-if [ ! -d /tmp/labautomation  ]; then
-	git clone https://github.com/learndevopsonline/labautomation.git /tmp/labautomation &>/dev/null
+if [ ! -d /tmp/automation  ]; then
+	git clone https://github.com/satishsatti3789/automation.git /tmp/automation &>/dev/null
 else
 	cd /tmp/labautomation
 	git stash &>/dev/null
@@ -17,9 +17,9 @@ fi
 
 echo -e "${Y}>>>>> Select a AWS Operation to Perform${N}"
 export PS3="Select Action> "
-cd /tmp/labautomation/aws
+cd /tmp/automation/aws
 select action in `ls -1 *.sh | sed -e 's/.sh$//'`; do
 		echo -e "\e[1;33m★★★ Performing $action ★★★\e[0m"
-		sh /tmp/labautomation/aws/$action.sh
+		sh /tmp/automation/aws/$action.sh
 		break
 done
